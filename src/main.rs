e1de8e49b59850eea02ec604082a912f00dfdbbc
lewisnjue <lewiskinyuanjue.ke@gmail.com>
2026-06-25 10:41:48 +0300

    able to read passed agurments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::env;
+use std::fs;
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let query = &args[1];
+    let file_path = &args[2];
+    println!("Searching {}", query);
+    println!("In file {}", file_path);
+
+    let content = fs::read_to_string(file_path)
+        .expect("should have been able to read the content of the file");
+    println!(
+        " THE CONTENT OF THE FILE IS \n =================================\n \n  {}",
+        content
+    );
 }
