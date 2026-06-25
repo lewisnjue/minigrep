7ffb69b5b396d4e7dc170ae9e5a720e1009ba510
lewisnjue <lewiskinyuanjue.ke@gmail.com>
2026-06-25 14:35:47 +0300

    fixed typo

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,12 @@ use std::env;
 use std::process;
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let config = minigre::Config::build(&args).unwrap_or_else(|err| {
+    let config = minigrep::Config::build(&args).unwrap_or_else(|err| {
         eprintln!("failed with error : {}", err);
 
         process::exit(1);
     });
-    if let Err(e) = minigre::run(config) {
+    if let Err(e) = minigrep::run(config) {
         eprintln!("failed with error : {}", e);
         process::exit(1);
     }
